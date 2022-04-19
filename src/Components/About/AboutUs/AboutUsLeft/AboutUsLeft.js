3a50a24c9d425ef1eddadc464d782a7e352e9a67
Faysal <frontendfaysal@gmail.com>
2022-04-20 02:19:37 +0600

    buttons changed to materail ui in about page

--- src/Components/About/AboutUs/AboutUsLeft/AboutUsLeft.js
+++ src/Components/About/AboutUs/AboutUsLeft/AboutUsLeft.js
@@ -6,9 +6,27 @@ const AboutUsLeft = () => {
         <div >
             <p style={{fontFamily:'cursive'}}>ABOUT US</p>
             <h2>Boosts Your Website Traffic!</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, eaque.</p>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id eaque reprehenderit tempore excepturi corrupti tenetur eum nostrum voluptatem atque dignissimos nobis facilis, voluptates alias obcaecati.</p>
-            <Button className='p-3' variant="contained">Our Services</Button>
+            <p style={{fontSize:'18px'}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, eaque.</p>
+            <p style={{fontSize:'18px'}}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id eaque reprehenderit tempore excepturi corrupti tenetur eum nostrum voluptatem atque dignissimos nobis facilis, voluptates alias obcaecati.</p>
+            <Button
+                    sx={{
+                        py: 2,
+                        px: 3,
+                        mr: 2,
+                        fontSize: '12px',
+                        fontWeight: '500',
+                        fontFamily: 'Rubik',
+                        border: '1px solid #03ef62',
+                        backgroundImage: 'linear-gradient(96deg, #03ef62 0%, #1bb41d 100%)',
+                        color: '#fff',
+                        '&:hover': {
+                            background: '#03ef62',
+                            color: '#fff',
+                            }
+                        }}
+                    >
+                    Discover More
+            </Button>
 
         </div>
     );
--- src/Components/About/WhyChooseUs/WhyChooseUsRight/WhyChooseUsRight.js
+++ src/Components/About/WhyChooseUs/WhyChooseUsRight/WhyChooseUsRight.js
@@ -18,7 +18,7 @@ const WhyChooseUsRight = () => {
 
             <div style={{display:'flex'}}>
                 <div>
-                    <img style={{width:'25px'}} src="https://i.ibb.co/Ns4LR4s/5290058.png" alt="right mark" />
+                    <img style={{width:'25px'}} src="https://i.ibb.co/92VNKSr/5290982.png" alt="right mark" />
                 </div>
 
                 <div className='ps-3'>
@@ -38,7 +38,26 @@ const WhyChooseUsRight = () => {
                 </div>
             </div>
 
-            <Button className='p-3 mt-4' variant="contained">Our Services</Button>
+            <Button
+                    sx={{
+                        py: 2,
+                        px: 3,
+                        mr: 2,
+                        fontSize: '12px',
+                        fontWeight: '500',
+                        fontFamily: 'Rubik',
+                        border: '1px solid #03ef62',
+                        backgroundImage: 'linear-gradient(96deg, #03ef62 0%, #1bb41d 100%)',
+                        color: '#fff',
+                        '&:hover': {
+                            background: '#03ef62',
+                            color: '#fff',
+                            }
+                        }}
+                    >
+                    Discover More
+            </Button>
+            
 
         </div>
         </div>
