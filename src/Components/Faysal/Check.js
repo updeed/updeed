977c69c37003b08be613fc53a03685790f3d2012
Faysal <frontendfaysal@gmail.com>
2022-04-19 13:49:11 +0600

    checking if all are okay

---
+++ src/Components/Faysal/Check.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Check = () => {
+    return (
+        <div>
+            <h2>This is for simple checking</h2>
+        </div>
+    );
+};
+
+export default Check;
