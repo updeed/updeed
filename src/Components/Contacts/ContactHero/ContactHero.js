26b13b392474f9555b13ec877da94041360236f1
Faysal <frontendfaysal@gmail.com>
2022-04-24 14:05:18 +0600

    needs image compressing

--- src/Components/Contacts/ContactHero/ContactHero.js
+++ src/Components/Contacts/ContactHero/ContactHero.js
@@ -6,7 +6,7 @@ const ContactHero = () => {
     return (
         <div className='contact-hero'>
             <div className='text-center blog-text'>
-                        <h2 className='text-white custom-title1'>Our Team</h2>
+                        <h2 className='text-white custom-title1'>Contact Us</h2>
                         <NavLink style={{textDecoration:'none' , color:'white'}} to="/home">Home</NavLink>
                 </div> 
         </div>
